0633aa5f2f2ecc997c689c3f6d7ce8688132bf95
xlDeadman <romerorojasalanjose@mail.com>
2026-05-21 20:18:47 -0600

    Fix CSRF

--- chicago_daily_news/settings.py
+++ chicago_daily_news/settings.py
@@ -89,3 +89,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-77117.up.railway.app']
