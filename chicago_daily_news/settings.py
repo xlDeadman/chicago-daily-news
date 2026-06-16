e9c55869b5cf97d2023a7c5b86d9cb1357996fb3
xlDeadman <romerorojasalanjose@gmail.com>
2026-06-16 08:48:19 -0600

    fecha

--- chicago_daily_news/settings.py
+++ chicago_daily_news/settings.py
@@ -75,7 +75,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
